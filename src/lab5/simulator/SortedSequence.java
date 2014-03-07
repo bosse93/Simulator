1bf642898e025c77f472f471ae130721faad6b2d
jonniv-6 <jonniv-6@student.ltu.se>
2014-03-07 12:55:46 +0100

    Lagt in en sorterings del. EJ FULLSTÄNDIG ej fått den att ta emot Object av typen Event än.

--- src/lab5/simulator/SortedSequence.java
+++ src/lab5/simulator/SortedSequence.java
@@ -1,31 +1,67 @@
 package lab5.simulator;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class SortedSequence {
-	ArrayList <Event> eventList = new ArrayList<Event>();
+	ArrayList<Event> eventList = new ArrayList<Event>();
+
+	// ArrayList<Event> sortedSequence = new ArrayList<Event>();
+	// static ArrayList<Integer> test = new ArrayList<Integer>();
 	
-	
-	public void SortedSequence(){
-		
+/*Takes a arraylist with events and makes a call to insertionsort which sorts the list in a 
+ * ascending order
+ * @param <E> type of stored elements.
+ */
+	public void sort(ArrayList<Event> list) {
+		insertionSort(list);
 	}
-	
-	public void addEvent(Event e){
+
+	private static void insertionSort(ArrayList<Event> list) {
+		int insertedValue;
+		int insertionPoint;
+
+		for (int i = 1; i < list.size(); i++) {
+			insertedValue = list.get(i); 	// kopierar värdet på som ska sättas in.
+										
+			insertionPoint = i; 			// var i arrayen värdet ska sätta in
+			
+			// Indexet på arraylisten där värdet ska sättas in måste vara större än noll och
+			// värdet på objectet som är innan insertionPoint ska vara större än
+			// värdet som ska sättas in.
+			// Om alla dessa uppfylls så sätts värdet på position
+			// Insertpoint och med värdet som var större.
+			// Detta upprepas tills insertionpoint är =0, eller om listan
+			// är rätt ordnad(alltså i rätt ordning,minst först och ökande).
+			while (insertionPoint > 0 && list.get(insertionPoint - 1) > insertedValue) {
+				list.set(insertionPoint, list.get(insertionPoint - 1));
+				insertionPoint--;
+			}
+			list.set(insertionPoint, insertedValue);
+		}
+	}
+
+	public void addEvent(Event e) {
 		add(e);
 	}
+
+	private void add(Event e) {
+		if (isEmpty()) {
+			eventList.add(0, e);
+		} else {
+			eventList.add(eventList.size(), e);
+			sort(eventList);
+		}
+
+	}
 	
-	private boolean empty(){ 
+
+	private boolean empty() {
 		return eventList.isEmpty();
 	}
-	public boolean isEmpty(){
+
+	public boolean isEmpty() {
 		return empty();
 	}
-	private void add(Event e) throws IllegalArgumentException{
-		if(e == null){
-			throw new NullPointerException();
-		}
-		if (isEmpty()){
-			eventList.add(e);
-		}
-	}
+
 }
